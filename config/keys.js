a4abad871c11301b7038651e3d914f6b59cf6b93
Quacer <quacerop@gmail.com>
2021-05-01 19:54:10 +0300

    fix

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //mongoURI: 'mongodb://vladilen:123456@ds111410.mlab.com:11410/fullstack',
+  mongoURI: 'mongodb+srv://12345:12345@cluster0.lyzs2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
+}
